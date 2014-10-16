8698f03a116b77001fd9fa3d25f5a8b22af1a877
ben-zen <benjf5+github@gmail.com>
2014-10-15 22:22:45 -0700

    Added formatting to uuid-gen.el, also moved from just presenting an almost-UUID to actually storing a UUID. Next step is to add struct-form UUID presentation, as well as Win32-struct-form UUID presentation.

--- uuid-gen.el
+++ uuid-gen.el
@@ -1,19 +1,63 @@
+;;; uuid-gen.el --- Tools to generate UUIDs for various uses.
+
 ;; Copyright (C) 2014 Ben Lewis
+
+;; Author: Ben Lewis <benjf5@gmail.com>
+;; Created: 13 Oct 2014
+;; Keywords: tools
 
 (random t)
 
-(defun gen-rand-num (mask)
+(defun uuid-gen-rand-num (mask)
   "Strips the lower two bytes out of a randomly-generated value."
   (logand (lsh (random) -16) mask))
+
+(defconst uuid-class-4
+  )
+
+
+(defun uuid-create-class-4 ()
+  (list
+   (uuid-gen-rand-num #xffffffff)
+   (uuid-gen-rand-num #xffff)
+   (logior (lsh (uuid-gen-rand-num #xffff) -4) #x4000)
+   (logior (lsh (uuid-gen-rand-num #xffff) -4)
+          (lsh (logior (uuid-gen-rand-num #x3) #x8) 12))
+   (list
+    (uuid-gen-rand-num #xff)
+    (uuid-gen-rand-num #xff)
+    (uuid-gen-rand-num #xff)
+    (uuid-gen-rand-num #xff)
+    (uuid-gen-rand-num #xff)
+    (uuid-gen-rand-num #xff)
+    (uuid-gen-rand-num #xff)
+    (uuid-gen-rand-num #xff))))
+
+(defun uuid-generate-string (uuid)
+  (concat
+   (format "%08X" (car uuid))
+   "-"
+   (format "%04X" (cadr uuid))
+   "-"
+   (format "%04X" (caddr uuid))
+   "-"
+   (format "%04X" (cadddr uuid))
+   "-"
+   (let
+       ((uuid-chars (car (cddddr uuid)))
+        (char-merge (lambda (char-list string)
+                      (if (null char-list)
+                          string
+                        (concat (format "%02X" (car char-list))
+                                (funcall char-merge (cdr char-list) string))))))
+     (funcall char-merge uuid-chars ""))))
 
 (defun insert-uuid ()
   "Generates a uuid (like '1F78D796-26FB-41C6-BCF3-E68AB900A627') and inserts it at the mark."
   (interactive)
   (insert
-   (format "%08X-%04X-%04X-%04X-%06X%06X"
-           (gen-rand-num #xffffffff)
-           (gen-rand-num #xffff)
-           (gen-rand-num #xffff)
-           (gen-rand-num #xffff)
-           (gen-rand-num #xffffff)
-           (gen-rand-num #xffffff))))
+   (uuid-generate-string (uuid-create-class-4))))
+
+(provide 'uuid-gen)
+
+;;; uuid-gen.el ends here
